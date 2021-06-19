d140219231786c02bb2d625347bc59d75ecc8ad0
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2021-06-18 18:21:09 -0700

    remove comment

--- src/extension.ts
+++ src/extension.ts
@@ -36,5 +36,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
