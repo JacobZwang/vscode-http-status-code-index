92d70089e798cf3b29b03c1e195deed0a538cf84
JacobZwang <59858341+JacobZwang@users.noreply.github.com>
2021-06-18 17:24:05 -0700

    add quick pick menu for status codes

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import statuses from "./statuses";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "http-status-code-index" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "http-status-code-index.search",
+    () => {
+      vscode.window
+        .showQuickPick(
+          statuses.map((status) => ({
+            label: status.code,
+            description: status.phrase,
+            detail: status.description,
+          })),
+          {
+            matchOnDescription: true,
+            matchOnDetail: true,
+          }
+        )
+        .then((item) => {
+          const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('http-status-code-index.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Http Status Code Index!');
-	});
+          if (item && editor?.selection.isEmpty) {
+            const pos = vscode.window.activeTextEditor?.selection.active;
 
-	context.subscriptions.push(disposable);
+            if (pos) {
+              editor?.edit((textEditor) => {
+                textEditor.insert(pos, item.label);
+              });
+            }
+          }
+        });
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
